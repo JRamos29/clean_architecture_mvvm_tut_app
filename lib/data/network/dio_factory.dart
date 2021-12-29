949a84c514a86214f8e0bf943e724d23578cc376
JRamos29 <malebolge@gmail.com>
2021-12-29 15:26:53 -0300

    Update DioFactory language with AppPreferences.

--- lib/data/network/dio_factory.dart
+++ lib/data/network/dio_factory.dart
@@ -2,6 +2,7 @@ import 'package:dio/dio.dart';
 import 'package:flutter/foundation.dart';
 import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 
+import '../../app/app_prefs.dart';
 import '../../app/constants.dart';
 
 const String APPLICATION_JSON = "application/json";
@@ -11,16 +12,21 @@ const String AUTHORIZATION = "authorization";
 const String DEFAULT_LANGUAGE = "language";
 
 class DioFactory {
+  AppPreferences _appPreferences;
+
+  DioFactory(this._appPreferences);
+
   Future<Dio> getDio() async {
     Dio dio = Dio();
 
     int _timeOut = 60 * 1000; // 1 min
+    String language = await _appPreferences.getAppLanguage();
 
     Map<String, String> headers = {
       CONTENT_TYPE: APPLICATION_JSON,
       ACCEPT: APPLICATION_JSON,
       AUTHORIZATION: Constants.token,
-      DEFAULT_LANGUAGE: "en" // TODO: get lang from app prefs
+      DEFAULT_LANGUAGE: language,
     };
 
     dio.options = BaseOptions(
