7478d3792452434933acdaaf057c353f70cf67af
JRamos29 <malebolge@gmail.com>
2021-12-29 12:12:24 -0300

    Creating extensions functions (Application Layer). Creating mappers and applying Null-Safety (Data Layer).

---
+++ lib/data/mapper/mapper.dart
@@ -0,0 +1,33 @@
+// Converts the response into non nullable object (model)
+
+import 'package:clean_architecture_mvvm_app/app/extensions.dart';
+import 'package:clean_architecture_mvvm_app/data/responses/responses.dart';
+import 'package:clean_architecture_mvvm_app/domain/authetication_model.dart';
+import 'package:clean_architecture_mvvm_app/domain/contacts_model.dart';
+import 'package:clean_architecture_mvvm_app/domain/customer_model.dart';
+
+const EMPTY = '';
+const ZERO = 0;
+
+extension CustomerResponseMapper on CustomerResponse? {
+  Customer toDomain() {
+    return Customer(
+        this?.id?.orEmpty() ?? EMPTY,
+        this?.name?.orEmpty() ?? EMPTY,
+        this?.numberOfNotificationsame?.orZero() ?? ZERO);
+  }
+}
+
+extension ContactsResponseMapper on ContactsResponse? {
+  Contacts toDomain() {
+    return Contacts(this?.phone?.orEmpty() ?? EMPTY,
+        this?.link?.orEmpty() ?? EMPTY, this?.email?.orEmpty() ?? EMPTY);
+  }
+}
+
+extension AuthenticationResponseMapper on AuthenticationResponse? {
+  Authentication toDomain() {
+    return Authentication(
+        this?.customer?.toDomain(), this?.contacts?.toDomain());
+  }
+}
--- lib/domain/authetication_model.dart
+++ lib/domain/authetication_model.dart
@@ -2,8 +2,8 @@ import 'contacts_model.dart';
 import 'customer_model.dart';
 
 class Authentication {
-  Customer customer;
-  Contacts contacts;
+  Customer? customer;
+  Contacts? contacts;
 
   Authentication(this.customer, this.contacts);
 }
