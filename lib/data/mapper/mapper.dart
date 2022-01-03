37f19ec3b484a4f1ac4c66f33c7e00a96121dbe2
JRamos29 <malebolge@gmail.com>
2022-01-03 17:53:55 -0300

    Adding Mappers fpr Home API (Data Layer).

--- lib/data/mapper/mapper.dart
+++ lib/data/mapper/mapper.dart
@@ -3,8 +3,12 @@
 import 'package:clean_architecture_mvvm_app/app/extensions.dart';
 import 'package:clean_architecture_mvvm_app/data/responses/responses.dart';
 import 'package:clean_architecture_mvvm_app/domain/models/authetication_model.dart';
+import 'package:clean_architecture_mvvm_app/domain/models/banner_model.dart';
 import 'package:clean_architecture_mvvm_app/domain/models/contacts_model.dart';
 import 'package:clean_architecture_mvvm_app/domain/models/customer_model.dart';
+import 'package:clean_architecture_mvvm_app/domain/models/home_model.dart';
+import 'package:clean_architecture_mvvm_app/domain/models/service_model.dart';
+import 'package:clean_architecture_mvvm_app/domain/models/store_model.dart';
 
 const EMPTY = "";
 const ZERO = 0;
@@ -35,5 +39,51 @@ extension AuthenticationResponseMapper on AuthenticationResponse? {
 extension ForgotPasswordResponseMapper on ForgotPasswordResponse? {
   String toDomain() {
     return this?.support?.orEmpty() ?? EMPTY;
+  }
+}
+
+extension ServiceResponseMapper on ServiceResponse? {
+  Service toDomain() {
+    return Service(this?.id?.orZero() ?? ZERO, this?.title?.orEmpty() ?? EMPTY,
+        this?.image?.orEmpty() ?? EMPTY);
+  }
+}
+
+extension StoreResponseMapper on StoreResponse? {
+  Store toDomain() {
+    return Store(this?.id?.orZero() ?? ZERO, this?.title?.orEmpty() ?? EMPTY,
+        this?.image?.orEmpty() ?? EMPTY);
+  }
+}
+
+extension BannerResponseMapper on BannerResponse? {
+  BannerAd toDomain() {
+    return BannerAd(this?.id?.orZero() ?? ZERO, this?.title?.orEmpty() ?? EMPTY,
+        this?.image?.orEmpty() ?? EMPTY, this?.link?.orEmpty() ?? EMPTY);
+  }
+}
+
+extension HomeResponseMapper on HomeResponse? {
+  HomeObject toDomain() {
+    List<Service> mappedServices =
+        (this?.data?.services?.map((service) => service.toDomain()) ??
+                Iterable.empty())
+            .cast<Service>()
+            .toList();
+
+    List<Store> mappedStores =
+        (this?.data?.stores?.map((store) => store.toDomain()) ??
+                Iterable.empty())
+            .cast<Store>()
+            .toList();
+
+    List<BannerAd> mappedBanners =
+        (this?.data?.stores?.map((bannerAd) => bannerAd.toDomain()) ??
+                Iterable.empty())
+            .cast<BannerAd>()
+            .toList();
+
+    var data = HomeData(mappedServices, mappedStores, mappedBanners);
+    return HomeObject(data);
   }
 }
