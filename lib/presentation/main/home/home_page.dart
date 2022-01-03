cc80f847f9fced5f7199574814e1bd1568cdc3f0
JRamos29 <malebolge@gmail.com>
2022-01-03 20:06:53 -0300

    Adding Banner Carousel widget (Presentation Layer). Adding Services widget (Presentation Layer). Adding Stores widget (Presentation Layer).

--- lib/presentation/main/home/home_page.dart
+++ lib/presentation/main/home/home_page.dart
@@ -1,6 +1,12 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:clean_architecture_mvvm_app/app/di.dart';
+import 'package:clean_architecture_mvvm_app/domain/models/banner_model.dart';
+import 'package:clean_architecture_mvvm_app/domain/models/service_model.dart';
+import 'package:clean_architecture_mvvm_app/domain/models/store_model.dart';
 import 'package:clean_architecture_mvvm_app/presentation/common/state_renderer/state_renderer_impl.dart';
 import 'package:clean_architecture_mvvm_app/presentation/main/home/home_viewmodel.dart';
+import 'package:clean_architecture_mvvm_app/presentation/resources/color_manager.dart';
+import 'package:clean_architecture_mvvm_app/presentation/resources/routes_manager.dart';
 import 'package:clean_architecture_mvvm_app/presentation/resources/strings_manager.dart';
 import 'package:clean_architecture_mvvm_app/presentation/resources/values_manager.dart';
 import 'package:flutter/material.dart';
@@ -71,15 +77,147 @@ class _HomePageState extends State<HomePage> {
   }
 
   Widget _getBannersCarousel() {
-    return Center();
+    return StreamBuilder<List<BannerAd>>(
+        stream: _viewModel.outputBanners,
+        builder: (context, snapshot) {
+          return _getBanner(snapshot.data);
+        });
+  }
+
+  Widget _getBanner(List<BannerAd>? banners) {
+    if (banners != null) {
+      return CarouselSlider(
+          items: banners
+              .map((banner) => SizedBox(
+                    width: double.infinity,
+                    child: Card(
+                      elevation: AppSize.s1_5,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(AppSize.s12),
+                          side: BorderSide(
+                              color: ColorManager.white, width: AppSize.s1_5)),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(AppSize.s12),
+                        child: Image.network(
+                          banner.image,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                  ))
+              .toList(),
+          options: CarouselOptions(
+            height: AppSize.s190,
+            autoPlay: true,
+            enableInfiniteScroll: true,
+            enlargeCenterPage: true,
+          ));
+    } else {
+      return Container();
+    }
   }
 
   Widget _getServices() {
-    return Center();
+    return StreamBuilder<List<Service>>(
+        stream: _viewModel.outputServices,
+        builder: (context, snapshot) {
+          return _getServicesWidget(snapshot.data);
+        });
+  }
+
+  Widget _getServicesWidget(List<Service>? services) {
+    if (services != null) {
+      return Padding(
+        padding: EdgeInsets.only(left: AppPadding.p12, right: AppPadding.p12),
+        child: Container(
+          height: AppSize.s140,
+          margin: EdgeInsets.symmetric(vertical: AppMargin.m12),
+          child: ListView(
+            scrollDirection: Axis.horizontal,
+            children: services
+                .map((service) => Card(
+                      elevation: AppSize.s4,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(AppSize.s12),
+                          side: BorderSide(
+                              color: ColorManager.white, width: AppSize.s1_5)),
+                      child: Column(
+                        children: [
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(AppSize.s12),
+                            child: Image.network(
+                              service.image,
+                              fit: BoxFit.cover,
+                              width: AppSize.s130,
+                              height: AppSize.s130,
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.only(top: AppPadding.p8),
+                            child: Align(
+                              alignment: Alignment.center,
+                              child: Text(
+                                service.title,
+                                textAlign: TextAlign.center,
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ))
+                .toList(),
+          ),
+        ),
+      );
+    } else {
+      return Container();
+    }
   }
 
   Widget _getStores() {
-    return Center();
+    return StreamBuilder<List<Store>>(
+        stream: _viewModel.outputStores,
+        builder: (context, snapshot) {
+          return _getStoresWidget(snapshot.data);
+        });
+  }
+
+  Widget _getStoresWidget(List<Store>? stores) {
+    if (stores != null) {
+      return Padding(
+        padding: EdgeInsets.only(
+            left: AppPadding.p12, right: AppPadding.p12, top: AppPadding.p12),
+        child: Flex(
+          direction: Axis.vertical,
+          children: [
+            GridView.count(
+              crossAxisSpacing: AppSize.s8,
+              mainAxisSpacing: AppSize.s8,
+              physics: ScrollPhysics(),
+              shrinkWrap: true,
+              crossAxisCount: 2,
+              children: List.generate(stores.length, (index) {
+                return InkWell(
+                  onTap: () {
+                    // navigate to store details screen
+                    Navigator.of(context).pushNamed(Routes.storeDetailsRoute);
+                  },
+                  child: Card(
+                    elevation: AppSize.s4,
+                    child: Image.network(
+                      stores[index].image,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                );
+              }),
+            )
+          ],
+        ),
+      );
+    } else {
+      return Container();
+    }
   }
 
   @override
